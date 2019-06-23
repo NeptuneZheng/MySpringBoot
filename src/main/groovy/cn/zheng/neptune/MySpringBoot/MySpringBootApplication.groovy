98386cc0b2ac74e7eba9adf79458e49250099ff5
Neptune Zheng <neptune.zhenge@oocl.com>
2019-06-23 16:01:52 +0800

    init my project

---
+++ src/main/groovy/cn/zheng/neptune/MySpringBoot/MySpringBootApplication.groovy
@@ -0,0 +1,13 @@
+package cn.zheng.neptune.MySpringBoot
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MySpringBootApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MySpringBootApplication, args)
+	}
+
+}
---
+++ src/test/groovy/cn/zheng/neptune/MySpringBoot/MySpringBootApplicationTests.groovy
@@ -0,0 +1,16 @@
+package cn.zheng.neptune.MySpringBoot
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class MySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
